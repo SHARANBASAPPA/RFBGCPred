80a498c5f8e9041e734ad7c85b87840d670a5cf7
SHARAN MADIVAL <smadival509@gmail.com>
2024-09-15 13:35:46 +0530

    Removed all existing files

--- rfbgcpred/__init__.py
+++
@@ -1,4 +0,0 @@
-# Mark this as a package
-from .cli import main
-from .model_utils import load_models, predict_bgc
-from .sequence_extraction import extract_sequence_from_fasta, extract_sequence_from_gbk
